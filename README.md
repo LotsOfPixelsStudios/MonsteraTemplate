071959a65798b45d5ec94f659ba39ecd1be226c2
12rcu <56599453+12rcu@users.noreply.github.com>
2023-11-10 20:07:29 +0100

    wip added resources

--- README.md
+++ README.md
@@ -3,11 +3,47 @@
 ## Licence Notice:
 
 This project is released under the Apache 2.0 licence although the kotlin library Monstera is released under GPL-3.0,
-as mentioned there, such a project like this, where the library is just "used" to create a minecraft addon, we can put 
+as mentioned there, such a project like this, where the library is just "used" to create a minecraft addon, we can put
 our own licence.
 
 ## Getting Started
 
 You can search project wide for the keyword "todo" to see what you should change.
 
-### Disable scripting:
+### Std Lib:
+
+#### No access
+
+If you want to use the std lib, you can write us an e-mail or contact the maintainer on GitHub.
+
+If you don't have access to the std lib, disable it by removing the following line from the `build.gradle.kts`
+
+````gradle
+implementation("com.lotsofpixelsstudios:monstera-std-lib:<version>")
+````
+
+Reload the gradle project and remove code that would throw an error.
+
+#### With access
+
+If you have access go to you're gradle home directory (on Windows: `%HOMEPATH%\.gradle`) and add a file named
+`env-timoliacreative.local.gradle.kts` and add the following line:
+
+````gradle
+project.extra["gitlab_token"] = "<you're access token>"
+````
+
+With you're access token and reload the gradle project.
+
+## Scripting
+
+Note: You need to activate scripting in the world (edit the world in minecraft and check the holiday creator features
+within the experiments)
+
+To disable scripting:
+
+- remove the `npm.cmd run build` command from the config (Main.kt)
+- remove the `scriptEntryFile` from the config (Main.kt)
+- remove the `scriptingVersion` from the config (Main.kt)
+- remove the `scripts()` function call from the addon (Main.kt)
+- optionally you can remove the whole scripting directory on the root
--- src/main/kotlin/com/lop/addon/Main.kt
+++ src/main/kotlin/com/lop/addon/Main.kt
@@ -1,24 +1,73 @@
 package com.lop.addon
 
 import com.lop.devtools.monstera.addon.addon
+import com.lop.devtools.monstera.addon.dev.zipper.zipWorld
 import com.lop.devtools.monstera.config
 import com.lop.devtools.monstera.files.File
 import com.lop.devtools.monstera.files.getResource
+import com.lop.devtools.stdlib.furnitures.furniture
+import com.lop.devtools.stdlib.furnitures.shared.FurnitureDropBehaviour
+import com.lop.devtools.stdlib.npcs.npc
+import com.lop.devtools.stdlib.packaging.packaging
+import com.lop.devtools.stdlib.player.Player
 import java.io.File
 
-
 fun main(args: Array<String>) {
-    val prop = addon(config(projectName = "Dungeon Rush") {
+    val conf = addon(config(projectName = "Template") { //todo
         "npm.cmd run build".runCommand(File(System.getProperty("user.dir"), "scripting"))
-        projectShort = "dr"
-        description = ""
+        projectShort = "te"
+        description = "sample"  //todo
         packIcon = getResource("general/pack.png")
-        world = getResource("world/dr_exp")
-        version = arrayListOf(0, 0, 1)
+        world = getResource("world/template-world")
+        version = arrayListOf(0, 1, 0)
         targetMcVersion = arrayListOf(1, 20, 0)
         scriptEntryFile = File("scripting", "dist", "src", "index.js")
         scriptingVersion = "1.6.0-beta"
     }) {
         scripts(directory = File("scripting", "dist", "src"))
+
+        if (args.contains("package")) {
+            packaging {
+                this.world = getResource("world/template-world")
+                addSkinPack {
+                    addSkin(getResource("skin_pack/alex_a.png"), true)
+                    addSkin(getResource("skin_pack/steve_s.png"), true)
+                }
+                addStoreArt { }
+                addMarketing { }
+                addBehaviorPack { }
+                addResourcePack { }
+            }
+        }
+
+        entity("sample_entity", "Sample Entity") {
+            resource {
+                textureLayer(getResource("entities/default_texture.png"))
+                geometryLayer(getResource("entities/default_model.geo.json"))
+            }
+            behaviour {
+                components {
+                    physics { }
+                }
+            }
+        }
+        item("sample_item", "display name") {
+            renderOffset("tool")
+            texture(getResource("item/default_texture.png"))
+        }
+        Player
+            .modify(this)
+            .modifyBehaviour { }
+
+        furniture("vase", "Vase", this) {
+            dropBehaviour = FurnitureDropBehaviour.CAN_PICKUP
+        }
+
+        npc("my_npc", "My NPC", this) {
+            //...
+        }
     }
+
+    if (args.contains("zip-world"))
+        zipWorld(conf, System.getenv("CI_PROJECT_NAME") ?: "local")
 }
